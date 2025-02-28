6f441c178972cc2ac7392a8f643db12e2b20e85c
Acer <Acer@LAPTOP-QO5NMR3E>
2025-02-28 22:04:59 +0530

    Add project files.

---
+++ Models/Products.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MyProject.Models
+{
+	public class Products
+	{
+        [Key]
+		public int ProductID { get; set; }
+
+        [Required(ErrorMessage ="Product Name Required.")]
+        [DisplayName("Product Name")]
+
+        public string ProductName { get; set; }
+
+        public int CategoryID { get; set; }
+
+        [Required(ErrorMessage = "Category Name Required.")]
+        public string CategoryName { get; set; }
+
+        public string Remarks { get; set; }
+    }
+
+    public class EFCodeFirstEntieis :DbContext
+    {
+         public DbSet<Products> products { get; set; }
+    }
+}
